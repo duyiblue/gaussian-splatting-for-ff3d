1266b0605a28b55cda1b941eab8f6371dd8b46c8
Yi Du <duyiblue@outlook.com>
2025-08-15 15:41:07 -0700

    modified the default tmp_dir

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 python train.py \
     -s /orion/u/duyi/recon3d/gaussian/backup/data/obj_000000 \
     -m output/test_no_depth \
+    --tmp_dir /orion/u/duyi/recon3d/gaussian/backup/tmp_dir \
     --test_iterations 100 5000 10000 20000 30000 \
     --save_iterations 100 5000 10000 20000 30000 \
     --disable_viewer \
