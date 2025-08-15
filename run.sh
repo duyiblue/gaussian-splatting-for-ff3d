c9ad346bf73e5cbcd354648548be58d8f1e2de79
Yi Du <duyiblue@outlook.com>
2025-08-15 00:21:44 -0700

    bash script for running

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 python train.py \
     -s /orion/u/duyi/recon3d/gaussian/backup/data/obj_000000 \
     -m output/test1 \
-    --geometry_only \
     --test_iterations 100 5000 10000 20000 30000 \
     --save_iterations 100 5000 10000 20000 30000 \
     --disable_viewer \
