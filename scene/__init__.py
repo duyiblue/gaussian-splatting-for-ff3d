50a345879e5a97777cb1428f3838afae2bcd018b
Yi Du <duyiblue@outlook.com>
2025-08-15 00:13:26 -0700

    make gaussian splatting compatible with our data format

--- scene/__init__.py
+++ scene/__init__.py
@@ -40,7 +40,11 @@ class Scene:
         self.train_cameras = {}
         self.test_cameras = {}
 
-        if os.path.exists(os.path.join(args.source_path, "sparse")):
+        if os.path.exists(os.path.join(args.source_path, "canonical_views_metadata.json")):
+            # Make FF3D our first choice!
+            print("Found canonical_views_metadata.json file, assuming FF3D data set!")
+            scene_info = sceneLoadTypeCallbacks["FF3D"](args.source_path, args.white_background, args.depths, args.eval)
+        elif os.path.exists(os.path.join(args.source_path, "sparse")):
             scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.depths, args.eval, args.train_test_exp)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
             print("Found transforms_train.json file, assuming Blender data set!")
