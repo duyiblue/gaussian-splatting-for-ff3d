84ef96b33ca032d6509f924280c9b914552d5eab
Yi Du <duyiblue@outlook.com>
2025-08-15 00:20:06 -0700

    bash script for running

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python train.py \
+    -s /orion/u/duyi/recon3d/gaussian/gaussian-splatting-for-ff3d/data/obj_000000 \
+    -m output/test1 \
+    --geometry_only \
+    --test_iterations 100 5000 10000 20000 30000 \
+    --save_iterations 100 5000 10000 20000 30000 \
+    --disable_viewer \
+    --checkpoint_iterations 100 5000 10000 20000 30000
