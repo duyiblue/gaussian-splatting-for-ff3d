9699c8e13948ffb546c683631cb1ac3dd9c97811
Yi Du <duyiblue@outlook.com>
2025-08-18 17:45:29 -0700

    Revert back to state after 5a51759

--- evaluate_ff3d.py
+++ evaluate_ff3d.py
@@ -267,7 +267,9 @@ def main():
             gt_data.append((gt_rgb, gt_depth, gt_mask))
             
             # Render view
-            rendered_data.append(render_view(camera, gaussians, pp, background))
+            rendered_rgb, rendered_depth, rendered_mask, rendered_invdepth = render_view(
+                camera, gaussians, pp, background)
+            rendered_data.append((rendered_rgb, rendered_depth, rendered_mask, rendered_invdepth))
             
             print(f"Processed view {camera.uid}")
         
