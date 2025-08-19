91f1099ed55a1cb26839c6dac9c3ab1dd4da99b2
Yi Du <duyiblue@outlook.com>
2025-08-19 16:22:08 -0700

    organize the scripts

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python evaluate_ff3d.py \
+python ../evaluate_ff3d.py \
     -s /orion/u/duyi/recon3d/gaussian/backup/data/obj_000000 \
     -m output/test_no_depth \
     --tmp_dir /orion/u/duyi/recon3d/gaussian/backup/tmp_dir \
